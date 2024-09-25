d947890aec51245ef1fac9bae4429e45f9c25a1c
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2024-09-25 16:55:47 +0200

    monitoing.c changes

--- philo/src/monitoring.c
+++ philo/src/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: maceccar <maceccar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 11:43:14 by lebartol          #+#    #+#             */
-/*   Updated: 2024/09/25 16:43:47 by maceccar         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:53:28 by maceccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static t_bool	check_meal(t_philo *philo);
 */
 void	monitor(t_data *data)
 {
-	while (!check_philo(data))
+	while (!check_philosophers(data))
 		usleep(2000);
 	if (check_meals(data))
 		ft_mutex_write(data->first_philo, "each philosopher is satisfied");
@@ -64,18 +64,19 @@ static t_philo	*check_philosophers(t_data *data)
 static t_bool	check_death(t_philo *philo)
 {
 	t_bool		is_dead;
-	static int	y;
+	static int	philos_satisfied;
 
 	if (philo->id == 1)
-		y = 0;
+		philos_satisfied = 0;
 	pthread_mutex_lock(&philo->philo_lock);
 	is_dead = ((int)(get_current_time() - philo->last_meal) >= philo->data->time_to_die + 5);
-	y += (philo->meals_eaten > philo->data->meals_count && philo->data->meals_count != -1);
+	if (philo->meals_eaten > philo->data->meals_count && philo->data->meals_count != -1)
+		philos_satisfied++;
 	pthread_mutex_unlock(&philo->philo_lock);
-	if (y == philo->data->number_of_philosophers)
-		return (1);
+	if (philos_satisfied == philo->data->number_of_philosophers)
+		return (true);
 	if (philo->id == philo->data->number_of_philosophers)
-		y = 0;
+		philos_satisfied = 0;
 	return (is_dead);
 }
 
@@ -136,7 +137,7 @@ static t_bool	check_meal(t_philo *philo)
 	int	is_satisfied;
 
 	pthread_mutex_lock(&philo->philo_lock);
-	is_satisfied = (philo->meals_eaten < philo->data->meals_count);
+	is_satisfied = (philo->meals_eaten == philo->data->meals_count);
 	pthread_mutex_unlock(&philo->philo_lock);
 	return (is_satisfied);
 }
